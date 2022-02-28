756bd1a19610f0fe938bf5065db40bb947fff878
corrooli <corrooli@students.zhaw.ch>
2022-02-28 13:32:58 +0100

    Create parameter container class

--- ard/ard.py
+++ ard/ard.py
@@ -6,38 +6,9 @@ from scipy.fftpack import idct, dct
 class ARDSimulator:
 
     # TODO Maybe create parameter class?
-    def __init__(self, room_size, src_pos, max_simulation_frequency, T, spatial_samples_per_wave_length=4, c=343, Fs=8000, verbose=False, visualize=False):
-        '''
-        Instantiates an ARD simulation session.
-
-        Parameters
-        ----------
-        room_size : ndarray
-            Size of the room in meters. Can be 1D, 2D or 3D.
-        src_pos : ndarray
-            Location of signal source inside the room. Can be 1D, 2D or 3D.
-        max_simulation_frequency : float
-            Uppermost frequency of simulation. Can be dialed in lower to enhance performance.
-        T : float
-            Simulation time [s].
-        spatial_samples_per_wave_length : int
-            Number of spatial samples per wave length. Usually 2 to 4. Lower values decrease 
-            resolution but enhances performance.
-        c : float
-            Speed of sound [m/s]. Depends on air temperature, pressure and humidity. 
-        Fs : int
-            Sampling rate. The higher, the more fidelity but lower performance.
-        verbose : boolean
-            Prints information on the terminal for debugging and status purposes.
-        visualize : boolean
-            Visualizes wave propagation in a plot.
-        '''
-        assert(len(room_size) >= 1), "Room dimensions should be bigger than 1D."
-        assert(len(room_size) <= 3), "Room dimensions should be lower than 3D."
-
-        self.room_size = room_size
-        self.src_pos = src_pos
-        self.max_simulation_frequency = max_simulation_frequency
+    def __init__(self, parameters):
+        # Parameter class instance
+        self.param = parameters
 
         # Array, which stores air pressure at each given point in time in the voxelized grid
         self.pressure_field = None
@@ -45,40 +16,18 @@ class ARDSimulator:
         # Array for pressure field results (auralisation and visualisation)
         self.pressure_field_results = []
 
-        self.c = c
-        self.Fs = Fs
-        self.T = T
-
-        # Calculating the number of samples the simulation takes.
-        self.number_of_samples = T * Fs
-
-        # Calculate time stepping (Δ_t)
-        self.delta_t = T / self.number_of_samples
-
-        # Voxel grid spacing. Changes according to frequency
-        #self.H = self.calculate_voxelization_step(
-       #     spatial_samples_per_wave_length)
-        
-        self.H = 0.1
-        
-        if verbose:
-            print(f"H = {self.H}")
-
-        # Longest room dimension length dividied by H (voxel grid spacing).
-        self.space_divisions = int(np.max(room_size) / self.H)
+        # Forces (result of DCT computation) TODO: Elaborate more
+        self.forces = None
 
         # Instantiate impulse array which keeps track of impulses in space over time.
         self.impulses = np.zeros(
-            shape=[self.number_of_samples, self.space_divisions])
+            shape=[self.param.number_of_samples, self.param.space_divisions])
         self.impulse_location = 0  #  TODO Put into constructor/parameter class
         self.dirac_a = 0.1  #  TODO Put into constructor/parameter class
 
         # Fill impulse array with impulses.  TODO: Switch between gaussian and dirac maybe?
-        self.impulses[:, int((self.space_divisions - 1) * (src_pos[0] / room_size[0]))] = [ARDSimulator.create_normalized_dirac_impulse(
-            self.dirac_a, t) for t in np.arange(0, T, self.delta_t)]
-
-        self.verbose = verbose
-        self.visualize = visualize
+        self.impulses[:, int((self.param.space_divisions - 1) * (self.param.src_pos[0] / self.param.room_size[0]))] = [ARDSimulator.create_normalized_dirac_impulse(
+            self.dirac_a, t) for t in np.arange(0, self.param.T, self.param.delta_t)]
 
     def preprocessing(self):
         '''
@@ -92,14 +41,15 @@ class ARDSimulator:
         '''
         # Step 1 a). Preparing pressure field. Equates to function p(x) on the paper.
         self.pressure_field = np.zeros(
-            shape=[len(self.room_size), self.space_divisions])
+            shape=[len(self.param.room_size), self.param.space_divisions])
 
         # Step 1 b). Rectangular decomposition. Skipped as of now.
         # TODO: Implement rectangular decomposition
 
         # Step 1 c). Precomputation for the DCTs to be performed. Transforming impulse to spatial
         # forces. Skipped partitions as of now. TODO: Implement partitions
-        self.forces = dct(self.impulses[1], n=self.space_divisions, type=1)
+        self.forces = dct(self.impulses[1],
+                          n=self.param.space_divisions, type=1)
 
     def simulation(self):
         '''
@@ -115,39 +65,41 @@ class ARDSimulator:
         # Relates to equation 5 and 8 of "An efficient GPU-based time domain solver for the
         # acoustic wave equation" paper.
         # For reference, see https://www.microsoft.com/en-us/research/wp-content/uploads/2016/10/4.pdf.
-        omega_i = self.c * np.pi * (np.linspace(0, np.max(self.room_size), self.space_divisions) / np.max(self.room_size))
+        omega_i = self.param.c * np.pi * \
+            (np.linspace(0, np.max(self.param.room_size),
+             self.param.space_divisions) / np.max(self.param.room_size))
 
         # Convert omega_i from row vector to column vector
         omega_i = omega_i.reshape([len(omega_i), 1])
 
         # Update time stepping. Relates to M^(n+1) and M^n in equation 8.
         # TODO Number of partitions missing atm
-        M_previous = np.zeros(shape=[self.space_divisions, 1])
+        M_previous = np.zeros(shape=[self.param.space_divisions, 1])
         M_current = np.zeros(shape=M_previous.shape)
 
         # Force field in spectral room. Temporary variable, right part after + sign of equation 8.
         force_field = np.zeros(shape=M_previous.shape)
 
-        if self.verbose:
+        if self.param.verbose:
             print(f"Shape of omega_i: {omega_i.shape}")
             print(f"Shape of pressure field: {self.pressure_field.shape}")
 
         # t_s: Time stepping.
-        for t_s in range(2, self.number_of_samples):
+        for t_s in range(2, self.param.number_of_samples):
             # Updating mode using the update rule in equation 8.
             # Relates to (2 * F^n) / (ω_i ^ 2) * (1 - cos(ω_i * Δ_t)) in equation 8.
-            force_field = ((2 * self.forces.reshape([self.space_divisions, 1])) / (
-                (omega_i + 0.00000001) ** 2)) * (1 - np.cos(omega_i * self.delta_t))
+            force_field = ((2 * self.forces.reshape([self.param.space_divisions, 1])) / (
+                (omega_i + 0.00000001) ** 2)) * (1 - np.cos(omega_i * self.param.delta_t))
             # TODO Perhaps set zero element to zero in force field if something goes horribly wrong
 
             # Relates to M^(n+1) in equation 8.
             M_next = 2 * M_current * \
-                np.cos(omega_i * self.delta_t) - M_previous + force_field
+                np.cos(omega_i * self.param.delta_t) - M_previous + force_field
 
             # Convert modes to pressure values using inverse DCT.
             self.pressure_field = idct(M_next.reshape(
-                self.space_divisions), n=self.space_divisions, type=1)
-            
+                self.param.space_divisions), n=self.param.space_divisions, type=1)
+
             self.pressure_field_results.append(self.pressure_field.copy())
 
             # Update time stepping to prepare for next time step / loop iteration.
@@ -155,70 +107,10 @@ class ARDSimulator:
             M_current = M_next.copy()
 
             # Execute DCT for next sample
-            self.forces = dct(self.impulses[t_s], n=self.space_divisions, type=1)
+            self.forces = dct(
+                self.impulses[t_s], n=self.param.space_divisions, type=1)
 
-    def calculate_voxelization_step(self, spatial_samples_per_wave_length):
-        '''
-        Calculate voxelization step for the segmentation of the room (voxelizing the scene).
-        The cell size is fixed by the chosen wave length and the number of spatial samples per 
-        wave length.
-        Calculation: ℎ <= 𝑐 / (2 * 𝑓_max)
-
-        Parameters
-        ----------
-        spatial_samples_per_wave_length : int
-            Number of spatial samples per wave length. Usually 2 to 4.
-
-        Returns
-        -------
-        float
-            ℎ, the voxelization step. In numerics and papers, it's usually referred to ℎ. 
-        '''
-        return self.c / (spatial_samples_per_wave_length * self.max_simulation_frequency)
-
-    @staticmethod
-    def create_dirac_impulse(a, x):
-        '''
-        Creates a dirac impulse, an infinitely thin and strong impulse.
-        For reference, see https://en.wikipedia.org/wiki/Dirac_delta_function.
-        TODO: Maybe create alternate method for gaussian white noise?
-
-        Parameters
-        ----------
-        a : float
-            Height / narrowness of impulse. The higher the value, the higher and narrower the
-            impulse.
-        x : float
-            Location coordinate
-        Returns
-        -------
-        float
-            δ(x), the calculated dirac impulse.
-        '''
-        return (1 / (np.sqrt(np.pi) * a)) * (np.exp(-((x ** 2) / (a ** 2))))
-
-
-    @staticmethod
-    def create_normalized_dirac_impulse(a, x):
-        '''
-        Generate dirac impulse which would have a peak of y=1 at x=0
-        Parameters
-        ----------
-        a : float
-            Height / narrowness of impulse. The higher the value, the higher and narrower the
-            impulse.
-            Note that the impulse height is normalized to 1 at its peak.
-        x : float
-            Location coordinate
-        Returns
-        -------
-        float
-            δ(x), the calculated dirac impulse.
-        '''
-        imp = ARDSimulator.create_dirac_impulse(a, x)
-        at_zero = ARDSimulator.create_dirac_impulse(a, 0)
-        return (imp / at_zero)
-
+    
 
     @staticmethod
     def update_rule(M, omega_i, delta_t, Fn):
@@ -244,6 +136,48 @@ class ARDSimulator:
             M_i ^ (n + 1)
         '''
         # TODO: Maybe use this?
+
+    @staticmethod
+    def create_dirac_impulse(a, x):
+        '''
+        Creates a dirac impulse, an infinitely thin and strong impulse.
+        For reference, see https://en.wikipedia.org/wiki/Dirac_delta_function.
+        TODO: Maybe create alternate method for gaussian white noise?
+
+        Parameters
+        ----------
+        a : float
+            Height / narrowness of impulse. The higher the value, the higher and narrower the
+            impulse.
+        x : float
+            Location coordinate
+        Returns
+        -------
+        float
+            δ(x), the calculated dirac impulse.
+        '''
+        return (1 / (np.sqrt(np.pi) * a)) * (np.exp(-((x ** 2) / (a ** 2))))
+
+    @staticmethod
+    def create_normalized_dirac_impulse(a, x):
+        '''
+        Generate dirac impulse which would have a peak of y=1 at x=0
+        Parameters
+        ----------
+        a : float
+            Height / narrowness of impulse. The higher the value, the higher and narrower the
+            impulse.
+            Note that the impulse height is normalized to 1 at its peak.
+        x : float
+            Location coordinate
+        Returns
+        -------
+        float
+            δ(x), the calculated dirac impulse.
+        '''
+        imp = ARDSimulator.create_dirac_impulse(a, x)
+        at_zero = ARDSimulator.create_dirac_impulse(a, 0)
+        return (imp / at_zero)
 
 
 '''
