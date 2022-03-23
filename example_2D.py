7fe26399e756dd2da81874e9c98da3ea4fde077b
corrooli <corrooli@students.zhaw.ch>
2022-03-23 19:56:18 +0100

    Correctly jiggles through the second interface
    Co-Authored-By: fuerbringer <severin@fuerbringer.info>
    Co-Authored-By: di2pf <vbknrzum@protonmail.com>

--- example_2D.py
+++ example_2D.py
@@ -32,10 +32,13 @@ sim_params = SIMP(
     visualize=visualize
 )
 
-partition_1 = PARTD(np.array([[int(c / 25)],[int(c / 25)]]), sim_params)
-partition_2 = PARTD(np.array([[int(c / 25)],[int(c / 25)]]), sim_params,do_impulse=False)
+SCALE = 50 # Scale of room. Gets calculated by speed of sound divided by SCALE
 
-part_data = [partition_1, partition_2]
+partition_1 = PARTD(np.array([[int(c / SCALE)],[int(c / SCALE)]]), sim_params)
+partition_2 = PARTD(np.array([[int(c / SCALE)],[int(c / SCALE)]]), sim_params,do_impulse=False)
+partition_3 = PARTD(np.array([[int(c / SCALE)],[int(c / SCALE)]]), sim_params,do_impulse=False)
+
+part_data = [partition_1, partition_2, partition_3]
 
 # Instantiating and executing simulation
 sim = ARDS(sim_params, part_data)
@@ -49,9 +52,9 @@ if True:
     ybtm = np.min(partition_1.pressure_field_results)
 
     fig = plt.figure(figsize=plt.figaspect(0.5))
-    ax_1 = fig.add_subplot(1, 2, 1, projection='3d')
-    
-    ax_2 = fig.add_subplot(1, 2, 2, projection='3d')
+    ax_1 = fig.add_subplot(2, 2, 1, projection='3d')
+    ax_2 = fig.add_subplot(2, 2, 2, projection='3d')
+    ax_3 = fig.add_subplot(2, 2, 4, projection='3d')
 
     cool_bionicle_X_1 = np.linspace(0, partition_1.space_divisions_x, partition_1.space_divisions_x)
     cool_bionicle_Y_1 = np.linspace(0, partition_1.space_divisions_y, partition_1.space_divisions_y)
@@ -61,41 +64,29 @@ if True:
     cool_bionicle_Y_2 = np.linspace(0, partition_2.space_divisions_y, partition_2.space_divisions_y)
     X_2, Y_2 = np.meshgrid(cool_bionicle_X_2, cool_bionicle_Y_2)
 
+    cool_bionicle_X_3 = np.linspace(0, partition_3.space_divisions_x, partition_3.space_divisions_x)
+    cool_bionicle_Y_3 = np.linspace(0, partition_3.space_divisions_y, partition_3.space_divisions_y)
+    X_3, Y_3 = np.meshgrid(cool_bionicle_X_3, cool_bionicle_Y_3)
+
     plot_limit = np.min(partition_2.pressure_field_results[:]), np.max(partition_2.pressure_field_results[:])
 
     for i in range(0, len(partition_1.pressure_field_results), 50):
-        '''
-        plt.clf()
-        plt.title(f"ARD 2D (t = {(sim_params.T * (i / sim_params.number_of_samples)):.4f}s)")
-        plt.subplot(1, 2, 1)
-        plt.plot(room_dims, partition_1.pressure_field_results[i], 'r', linewidth=1)
-        plt.ylim(top=ytop)
-        plt.ylim(bottom=ybtm)
-        plt.vlines(np.min(room_dims), ybtm, ytop, color='gray')
-        plt.vlines(np.max(room_dims), ybtm, ytop, color='gray')
-        plt.subplot(1, 2, 2)
-        plt.plot(room_dims, partition_2.pressure_field_results[i], 'b', linewidth=1)
-        plt.xlabel("Position [m]")
-        plt.ylabel("Displacement")
-        plt.ylim(top=ytop)
-        plt.ylim(bottom=ybtm)
-        plt.vlines(np.min(room_dims), ybtm, ytop, color='gray')
-        plt.vlines(np.max(room_dims), ybtm, ytop, color='gray')
-        plt.grid()
-        plt.pause(0.001)
-        '''
         Z_1 = partition_1.pressure_field_results[i]
         Z_2 = partition_2.pressure_field_results[i]
+        Z_3 = partition_3.pressure_field_results[i]
 
         ax_1.cla()
         ax_2.cla()
+        ax_3.cla()
 
-        plt.title(f"Look at my cool Bionicle© product: (t = {(sim_params.T * (i / sim_params.number_of_samples)):.4f}s)")
+        plt.title(f"t = {(sim_params.T * (i / sim_params.number_of_samples)):.4f}s")
         ax_1.plot_surface(X_1, Y_1, Z_1, cmap=coom.coolwarm, antialiased=False)
         ax_2.plot_surface(X_2, Y_2, Z_2, cmap=coom.coolwarm, antialiased=False)
+        ax_3.plot_surface(X_3, Y_3, Z_3, cmap=coom.coolwarm, antialiased=False)
 
         ax_1.set_zlim(plot_limit)
         ax_2.set_zlim(plot_limit)
+        ax_3.set_zlim(plot_limit)
 
         plt.pause(0.005)
 
--- pytARD_2D/ard.py
+++ pytARD_2D/ard.py
@@ -40,7 +40,8 @@ class ARDSimulator:
         )
 
         # TODO: Unify h of partition data, atm it's hard coded to first partition
-        self.FDTD_COEFFS = fdtd_coeffs_not_normalized * ((sim_parameters.c / part_data[0].h_x) ** 2)
+        self.FDTD_COEFFS_X = fdtd_coeffs_not_normalized * ((sim_parameters.c / part_data[0].h_x) ** 2)
+        self.FDTD_COEFFS_Y = fdtd_coeffs_not_normalized * ((sim_parameters.c / part_data[0].h_y) ** 2)
 
         # FDTD kernel size.
         self.FDTD_KERNEL_SIZE = int((len(fdtd_coeffs_not_normalized[0])) / 2) 
@@ -97,30 +98,51 @@ class ARDSimulator:
                 # Update impulses
                 self.part_data[i].new_forces = self.part_data[i].impulses[t_s].copy()
 
-            # Interface handling
+            # Interface handling Left -> Right (through y axis)
             for y in range(self.part_data[i].space_divisions_y):
-
-                pressure_field_around_interface = np.zeros(shape=[2 * self.FDTD_KERNEL_SIZE, 1])
+                pressure_field_around_interface_y = np.zeros(shape=[2 * self.FDTD_KERNEL_SIZE, 1])
 
                 # Left room
-                pressure_field_around_interface[0 : self.FDTD_KERNEL_SIZE] = self.part_data[0].pressure_field[y, -self.FDTD_KERNEL_SIZE : ].copy().reshape([self.FDTD_KERNEL_SIZE, 1])
+                pressure_field_around_interface_y[0 : self.FDTD_KERNEL_SIZE] = self.part_data[0].pressure_field[y, -self.FDTD_KERNEL_SIZE : ].copy().reshape([self.FDTD_KERNEL_SIZE, 1])
 
-                # Right room
-                pressure_field_around_interface[self.FDTD_KERNEL_SIZE : 2 * self.FDTD_KERNEL_SIZE] = self.part_data[1].pressure_field[y, 0 : self.FDTD_KERNEL_SIZE].copy().reshape(self.FDTD_KERNEL_SIZE, 1)
+                # Right top room
+                pressure_field_around_interface_y[self.FDTD_KERNEL_SIZE : 2 * self.FDTD_KERNEL_SIZE] = self.part_data[1].pressure_field[y, 0 : self.FDTD_KERNEL_SIZE].copy().reshape(self.FDTD_KERNEL_SIZE, 1)
 
-                new_forces_from_interface = self.FDTD_COEFFS.dot(pressure_field_around_interface)
+                # Calculate new forces transmitted into room
+                new_forces_from_interface_y = self.FDTD_COEFFS_Y.dot(pressure_field_around_interface_y)
 
-                self.part_data[0].new_forces[y, -3] += new_forces_from_interface[0]
-                self.part_data[0].new_forces[y, -2] += new_forces_from_interface[1]
-                self.part_data[0].new_forces[y, -1] += new_forces_from_interface[2]
-                self.part_data[1].new_forces[y, 0] += new_forces_from_interface[3]
-                self.part_data[1].new_forces[y, 1] += new_forces_from_interface[4]
-                self.part_data[1].new_forces[y, 2] += new_forces_from_interface[5]
+                # Add everything together
+                self.part_data[0].new_forces[y, -3] += new_forces_from_interface_y[0]
+                self.part_data[0].new_forces[y, -2] += new_forces_from_interface_y[1]
+                self.part_data[0].new_forces[y, -1] += new_forces_from_interface_y[2]
+                self.part_data[1].new_forces[y, 0] += new_forces_from_interface_y[3]
+                self.part_data[1].new_forces[y, 1] += new_forces_from_interface_y[4]
+                self.part_data[1].new_forces[y, 2] += new_forces_from_interface_y[5]
         
+            # Interface handling Right -> Bottom (through x axis)
+            for x in range(self.part_data[i].space_divisions_x):
+                pressure_field_around_interface_x = np.zeros(shape=[2 * self.FDTD_KERNEL_SIZE, 1])
+
+                # Right top room
+                pressure_field_around_interface_x[0 : self.FDTD_KERNEL_SIZE] = self.part_data[1].pressure_field[-self.FDTD_KERNEL_SIZE : , x].copy().reshape([self.FDTD_KERNEL_SIZE, 1])
+
+                # Right bottom room
+                pressure_field_around_interface_x[self.FDTD_KERNEL_SIZE : 2 * self.FDTD_KERNEL_SIZE] = self.part_data[2].pressure_field[0 : self.FDTD_KERNEL_SIZE, x].copy().reshape(self.FDTD_KERNEL_SIZE, 1)
+
+                # Calculate new forces transmitted into room
+                new_forces_from_interface_x = self.FDTD_COEFFS_X.dot(pressure_field_around_interface_x)
+
+                # Add everything together
+                self.part_data[1].new_forces[-3, x] += new_forces_from_interface_x[0]
+                self.part_data[1].new_forces[-2, x] += new_forces_from_interface_x[1]
+                self.part_data[1].new_forces[-1, x] += new_forces_from_interface_x[2]
+                self.part_data[2].new_forces[0, x] += new_forces_from_interface_x[3]
+                self.part_data[2].new_forces[1, x] += new_forces_from_interface_x[4]
+                self.part_data[2].new_forces[2, x] += new_forces_from_interface_x[5]
+
         # Microphones. TODO: Make mics dynamic
         self.mic1.write_to_file(self.sim_param.Fs)
         self.mic2.write_to_file(self.sim_param.Fs)
-
 
 
 '''
