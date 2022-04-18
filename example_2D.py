Fixed CFL calculation. Everything works now

example_2D.py
@@ -12,16 +12,16 @@ import numpy as np
 
 # Room parameters
 duration = 1  #  seconds
-Fs = 8000  # sample rate
-upper_frequency_limit = Fs/6  # Hz
+Fs = 10000  # sample rate
+upper_frequency_limit = Fs/20  # Hz
 c = 342  # m/s
-spatial_samples_per_wave_length = 6
+spatial_samples_per_wave_length = 4
 
 # Procedure parameters
 verbose = True
 auralize = True
 visualize = True
-write_to_file = True
+write_to_file = False
 compress_file = True
 
 # Compilation of room parameters into parameter class
@@ -39,9 +39,9 @@ SCALE = 60  # Scale of room. Gets calculated by speed of sound divided by SCALE
 
 # Define impulse that gets emitted into the room. Uncomment which kind of impulse you want
 impulse_location = np.array([[int((c / SCALE) / 2)], [int((c / SCALE) / 2)]])
-# impulse = Gaussian(sim_params, impulse_location, 10000)
-# impulse = Unit(sim_params, impulse_location, 1)
-impulse = WaveFile(sim_param, impulse_location, 'clap_8000.wav', 100)
+# impulse = Gaussian(sim_param, impulse_location, 10000)
+impulse = Unit(sim_param, impulse_location, 1, upper_frequency_limit-1)
+#impulse = WaveFile(sim_param, impulse_location, 'clap_8000.wav', 100)
 
 # Paritions of the room. Can be 1..n. Add or remove rooms here.
 partition_1 = PARTD(np.array([[int(c / SCALE)], [int(c / SCALE)]]), sim_param, impulse)
@@ -87,10 +87,10 @@ if auralize:
     # Instantiation serializer for reading and writing simulation state data
     serializer = Serializer(compress=compress_file)
 
-    # Instantiating and executing simulation
-    sim = ARDS(sim_param, part_data, 1, interfaces, mics)
-    sim.preprocessing()
-    sim.simulation()
+# Instantiating and executing simulation
+sim = ARDS(sim_param, part_data, 1, interfaces, mics)
+sim.preprocessing()
+sim.simulation()
 
 # Write partitions and state data to disk
 if write_to_file:

example_3D.py
@@ -13,10 +13,10 @@ import numpy as np
 
 # Room parameters
 duration = 1 # seconds
-Fs = 8000 # sample rate
-upper_frequency_limit = Fs / 10 # Hz
+Fs = 10000 # sample rate
+upper_frequency_limit = Fs / 22 # Hz
 c = 342 # m/s
-spatial_samples_per_wave_length = 6
+spatial_samples_per_wave_length = 4
 
 # Procedure parameters
 verbose = True
@@ -49,9 +49,9 @@ impulse_location = np.array([
 ])
 
 # Define impulse location that gets emitted into the room
-# impulse = Gaussian(sim_params, impulse_location, 1)
+# impulse = Gaussian(sim_param, impulse_location, 1)
 impulse = Unit(sim_param, impulse_location, 1, upper_frequency_limit - 1)
-# impulse = WaveFile(sim_params, impulse_location, 'clap.wav', 100) # Uncomment for wave file injection
+# impulse = WaveFile(sim_param, impulse_location, 'clap.wav', 100) # Uncomment for wave file injection
 
 partition_1 = PARTD(np.array([
     [int(c / SCALE)], # X, width

pytARD_2D/partition_data.py
@@ -30,11 +30,12 @@ class PartitionData:
 
         # Voxel grid spacing. Changes automatically according to frequency
         self.h_y = SimulationParameters.calculate_voxelization_step(sim_param) 
-        self.h_x = SimulationParameters.calculate_voxelization_step(sim_param) # TODO: Remove h(y)?
+        self.h_x = SimulationParameters.calculate_voxelization_step(sim_param)
 
         # Check stability of wave equation
-        CFL = (sim_param.c * sim_param.delta_t) / self.h_x
-        assert(CFL <= 1), f"Courant-Friedrichs-Lewy number (CFL = {CFL}) is greater than 1. Wave equation is unstable. Try using a higher sample rate or more spatial samples per wave length."
+        CFL = sim_param.c * sim_param.delta_t * ((1 / self.h_x) + (1 / self.h_y))
+        CFL_target = np.sqrt(1/3)
+        assert(CFL <= CFL_target), f"Courant-Friedrichs-Lewy number (CFL = {CFL}) is greater than {CFL_target}. Wave equation is unstable. Try using a higher sample rate or more spatial samples per wave length."
         
         # Longest room dimension length dividied by H (voxel grid spacing).
         self.space_divisions_y = int(dimensions[1] / self.h_y)

pytARD_3D/partition_data.py
@@ -31,8 +31,9 @@ class PartitionData:
         self.h_x = SimulationParameters.calculate_voxelization_step(sim_param)
 
         # Check stability of wave equation
-        CFL = (sim_param.c * sim_param.delta_t) / self.h_x
-        assert(CFL <= 1), f"Courant-Friedrichs-Lewy number (CFL = {CFL}) is greater than 1. Wave equation is unstable. Try using a higher sample rate or more spatial samples per wave length."
+        CFL = sim_param.c * sim_param.delta_t * ((1 / self.h_x) + (1 / self.h_y) + (1 / self.h_z))
+        CFL_target = np.sqrt(1/3)
+        assert(CFL <= CFL_target), f"Courant-Friedrichs-Lewy number (CFL = {CFL}) is greater than {CFL_target}. Wave equation is unstable. Try using a higher sample rate or more spatial samples per wave length."
 
         # Longest room dimension length dividied by H (voxel grid spacing).
         self.space_divisions_z = int(dimensions[2] / self.h_z)
@@ -66,7 +67,7 @@ class PartitionData:
 
         if sim_param.verbose:
             print(
-                f"Created partition with dimensions {self.dimensions[0]}x{self.dimensions[1]}x{self.dimensions[2]} m\nℎ (z): {self.h_z}, ℎ (y): {self.h_y}, ℎ (x): {self.h_x} | Space divisions: {self.space_divisions_y}")
+                f"Created partition with dimensions {self.dimensions[0]}x{self.dimensions[1]}x{self.dimensions[2]} m\nℎ (z): {self.h_z}, ℎ (y): {self.h_y}, ℎ (x): {self.h_x} | Space divisions: {self.space_divisions_y} | CFL = {CFL}")
 
     def preprocessing(self):
         '''