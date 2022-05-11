a233ad149b9dc192c008c37f5836bd87b2e364b9
smailnik <smailnik@students.zhaw.ch>
2022-05-11 22:17:52 +0200

    Add plots for stencils.

--- common/finite_differences.py
+++ common/finite_differences.py
@@ -44,6 +44,38 @@ def get_laplacian_matrix(derivative, accuracy):
 
 
 if __name__ == '__main__':
+    plt.close('all')
+    
+    fig, ax = plt.subplots(ncols=2, nrows=2, sharex=True, sharey=True, figsize=(10, 5))
+    fig.suptitle('First Order')
+    for ind, current_ax in enumerate(FD_COEFFICIENTS[1]):
+        print(ind)
+        current_ax = ax.flatten()[ind]
+        curren_fdc = list(FD_COEFFICIENTS[1].values())[ind]
+        num_pts = len(curren_fdc)
+        current_ax.set_title(f"{num_pts} point stencil")
+        current_ax.plot((np.arange(num_pts) - int(num_pts/2)), curren_fdc, '-o')
+        current_ax.set_xlabel('dx')
+        current_ax.set_ylabel('weight')
+        current_ax.set_xlim(-8, 8)
+        current_ax.set_ylim(-1, 1)
+        
+    fig, ax = plt.subplots(ncols=2, nrows=3, sharex=True, sharey=True, figsize=(10, 5))
+    fig.suptitle('Laplacian')
+    for ind, current_ax in enumerate(FD_COEFFICIENTS[2]):
+        print(ind)
+        current_ax = ax.flatten()[ind]
+        curren_fdc = list(FD_COEFFICIENTS[2].values())[ind]
+        num_pts = len(curren_fdc)
+        current_ax.set_title(f"{num_pts} point stencil")
+        current_ax.plot((np.arange(num_pts) - int(num_pts/2)), curren_fdc, '-o')
+        current_ax.set_xlabel('dx')
+        current_ax.set_ylabel('weight')
+        current_ax.set_xlim(-8, 8)
+        current_ax.set_ylim(min(curren_fdc)-0.5, max(curren_fdc)+0.5)
+    ax[2,1].axis('off')
+    
+    plt.figure(3)
     K = get_laplacian_matrix(2, 6)
     plt.imshow(K)
     plt.show()
